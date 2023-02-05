1570ae4780f5e979128d34b8769ef24aa9870edb
Vincenza Paradiso <122514035+vinniparadiso@users.noreply.github.com>
2023-02-05 13:02:29 -0500

    Add files via upload

---
+++ HW1_CPSC.py
@@ -0,0 +1,43 @@
+Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#Vincenza Paradiso
+#PROBLEM 1
+type ("Rosewater")
+<class 'str'>
+type(0.001)
+<class 'float'>
+type (37)
+<class 'int'>
+>>> type(False)
+<class 'bool'>
+>>> #PROBLEM 2
+>>> #Here I used the "," to show spaces
+>>> print("My", "name", "is", "Vinni!",)
+My name is Vinni!
+>>> #Here I used the "+" to connect the words with so spaces
+>>> print("My"+"name"+"is"+"Vinni!")
+MynameisVinni!
+>>> #PROBLEM 3
+>>> int(3.7)
+3
+>>> float(27.7)
+27.7
+>>> float(27)
+27.0
+>>> #PROBLEM 4
+>>> print("Hello, My name is Vinni! \nI have 4 older sisters and I am from Columbus, OH. \nI have an English Mastiff named Guido. \nI love all kinds of music and singing with my sisters and parents. \nI am a Design Major and a Comp Sci minor. \nI am hoping to be  graphid designer or website designer when i graduate!")
+Hello, My name is Vinni! 
+I have 4 older sisters and I am from Columbus, OH. 
+I have an English Mastiff named Guido. 
+I love all kinds of music and singing with my sisters and parents. 
+I am a Design Major and a Comp Sci minor. 
+I am hoping to be  graphid designer or website designer when i graduate!
+>>> #PROBLEM 5
+>>> flour=2
+>>> milk=1
+>>> egg=4
+>>> oil=0.4
+vanilla=0.012
+cake=2*flour+0.5*(milk-egg+vanilla)+oil**2
+print(cake)
+2.6660000000000004
